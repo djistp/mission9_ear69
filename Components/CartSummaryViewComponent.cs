70305143bc4350a939c0d55ffa0cbbdd1c28ea56
Eduardo Rodriguez <eduardoarodriguez272@gmail.com>
2023-03-13 02:00:31 -0600

    donner

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mission9_ear69.Models;
+namespace mission9_ear69.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket basketS)
+        {
+            basket = basketS;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
